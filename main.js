98479c9e8d9834338ce9acb0032580ce8bbbcd7c
Pasquale Paudice <pasquale.paudice10@gmail.com>
2019-11-18 14:36:28 +0100

    js+

--- main.js
+++ main.js
@@ -4,12 +4,16 @@
 var cognomi = ['rossi', 'biondi', 'tozzi', 'stella'];
 
 console.log('array originario: ' + cognomi);
+console.log(' ');
 
 // input aggiunta valore nell' array
 
 var utente = prompt('Inserisci il tuo cognome');
+
  cognomi.push(utente);
+
 console.log('array con aggiunta utente: ' + cognomi);
+console.log(' ');
 
 // Ordinamento
 
@@ -29,6 +33,7 @@ for (var i = 0; i < cognomi.length ; i++){
 }
 
 console.log('array ordinato: ' + cognomi);
+console.log(' ');
 
 // Ricerca
 
@@ -40,6 +45,7 @@ for (var index = 0; index < cognomi.length ; index++){
 
         var posizione = index + 1;
         console.log('Il tuo cognome è nella posizione: ' + posizione);
+
     }
 
 }
